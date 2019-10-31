2e4d8aad1b6dc995fb87efa6dc216f8a643119a9
alejandro.iglesias <alejandro.iglesias@trifenix.io>
2019-10-31 19:15:36 -0300

    Modelo de entidades para la agregación de productos, dosis, enfermedades, entidades de certificación y objetos locales (no se guardan en la base de datos).

--- trifenix.agro.model/agro/Sickness.cs
+++ trifenix.agro.model/agro/Sickness.cs
@@ -3,18 +3,11 @@ using Cosmonaut.Attributes;
 
 namespace trifenix.agro.db.model.agro
 {
-    [SharedCosmosCollection("agro", "Ingredient")]
-    public class Ingredient : DocumentBase, ISharedCosmosEntity
+    [SharedCosmosCollection("agro", "Sickness")]
+    public class Sickness : DocumentBase, ISharedCosmosEntity
     {
         public override string Id { get; set; }
 
         public string Name { get; set; }
-
-        public IngredientCategory Category { get; set; }
-
-        
-
-
-
     }
 }
