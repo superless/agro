2e1bb97151a266e918cdfb2a946b2412ab6575a2
alejandro.iglesias <alejandro.iglesias@trifenix.io>
2019-10-31 19:14:11 -0300

    Añadido el elemento dosis como entrada, el que tiene el modelo que usará el cliente para enviar al servidor.

---
+++ trifenix.agro.model.external/Input/DosesInput.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+
+namespace trifenix.agro.model.external.Input
+{
+    public class DosesInput
+    {
+        public string[] IdVarieties { get; set; }
+
+        public string IdSpecie { get; set; }
+
+        public string[] IdsSickness { get; set; }
+
+        public int DaysToReEntryToBarrack { get; set; }
+
+        
+        public int ApplicationDaysInterval { get; set; }
+
+
+        
+        public int NumberOfSecuencialAppication { get; set; }
+
+
+        
+        public int WettingRecommended { get; set; }
+
+        private List<WaitingHarvestInput> _waitingToHarvest;
+
+        public List<WaitingHarvestInput> WaitingToHarvest
+        {
+            get
+            {
+                _waitingToHarvest = _waitingToHarvest ?? new List<WaitingHarvestInput>();
+                return _waitingToHarvest;
+            }
+            set { _waitingToHarvest = value; }
+        }
+
+    }
+
+    public class WaitingHarvestInput {
+        public int WaitingDays { get; set; }
+
+        public bool IsLabel { get; set; }
+
+        public string IdCertifiedEntity { get; set; }
+
+    }
+}
