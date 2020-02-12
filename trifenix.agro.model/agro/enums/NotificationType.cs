11e51b9c12fdee405d0d204f78809db051cb0fb7
Cristian Rojas <cristian.rojas.uv@trifenix.io>
2020-02-12 01:04:40 -0300

    Correccion error en eventos no fenologicos

--- trifenix.agro.model/agro/enums/NotificationType.cs
+++ trifenix.agro.model/agro/enums/NotificationType.cs
@@ -1,8 +1,8 @@
 ﻿namespace trifenix.agro.db.model.agro.enums
 {
     public enum NotificationType {
-        Default = 0,
-        Phenological = 1,
+        Default = 1,
+        Phenological = 0,
     }
 
 }
