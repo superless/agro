6db9201a3ca9399c33d68f5f3079f7643bf1edd0
Cristian Rojas <cristian.rojas.uv@trifenix.io>
2020-02-26 16:49:57 -0300

    Actualizado EntityIdRelated

--- trifenix.agro.enums/EntityIdRelated.cs
+++ trifenix.agro.enums/EntityIdRelated.cs
@@ -1,5 +1,5 @@
-﻿namespace trifenix.agro.enums
-{
+﻿namespace trifenix.agro.enums {
+
     public enum EntityRelated { 
         CERTIFIED_ENTITY = 0,
         SPECIE=1,
@@ -18,14 +18,9 @@
         CATEGORY_INGREDIENT=14,
         PHENOLOGICAL_EVENT=15,
 
-
-
-
-
     }
 
-    public enum PropertyRelated
-    {
+    public enum PropertyRelated {
         PRODUCT_BRAND = 0,
         USER_RUT = 1,
         USER_EMAIL=2,
@@ -36,17 +31,12 @@
         BUSINESSNAME_EMAIL=7,
         BUSINESSNAME_RUT = 8,
         BUSINESSNAME_GIRO = 9,
-        SPECIE_CODE = 10
-        
-
-
-
-
+        SPECIE_CODE = 10,
+        GENERIC_NAME = 11,
 
     }
 
-    public enum EnumerationRelated
-    {
+    public enum EnumerationRelated {
         ORDER_TYPE= 1,
         SEASON_CURRENT=2,
 
@@ -56,8 +46,5 @@
         DEFAULT =0,
         PHENOLOGICAL =1
     }
-
-
-
 
 }
