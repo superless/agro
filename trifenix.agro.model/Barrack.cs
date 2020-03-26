bf5a8c8c3c6f6d86a11331fb86d5ffa8e3168357
alejandro.iglesias <alejandro.iglesias@trifenix.io>
2020-03-26 18:43:44 -0300

    Barrack and Certified Entiity Added

--- trifenix.agro.model/Barrack.cs
+++ trifenix.agro.model/Barrack.cs
@@ -7,15 +7,20 @@ using trifenix.agro.enums.searchModel;
 namespace trifenix.agro.db.model.agro {
 
     [SharedCosmosCollection("agro", "Barrack")]
+    [ReferenceSearch(EntityRelated.BARRACK)]
     public class Barrack : DocumentBaseName, ISharedCosmosEntity {
         public override string Id { get; set; }
 
+        [StringSearch(StringRelated.GENERIC_NAME)]
         public override string Name { get; set; }
 
+        [ReferenceSearch(EntityRelated.SEASON)]
         public string SeasonId { get; set; }
 
+        [ReferenceSearch(EntityRelated.PLOTLAND)]
         public string IdPlotLand { get; set; }
 
+        
         public double Hectares { get; set; }
 
         public int PlantingYear { get; set; }
--- trifenix.agro.model/CertifiedEntity.cs
+++ trifenix.agro.model/CertifiedEntity.cs
@@ -1,5 +1,7 @@
 ﻿using Cosmonaut;
 using Cosmonaut.Attributes;
+using trifenix.agro.attr;
+using trifenix.agro.enums.searchModel;
 
 namespace trifenix.agro.db.model.agro {
 
@@ -7,6 +9,7 @@ namespace trifenix.agro.db.model.agro {
     /// Entidad certificadora, encargada de validar el proceso de exportación
     /// </summary>
     [SharedCosmosCollection("agro", "CertifiedEntity")]
+    [ReferenceSearch(EntityRelated.CERTIFIED_ENTITY)]
     public class CertifiedEntity : DocumentBaseName, ISharedCosmosEntity {
 
         /// <summary>
@@ -17,8 +20,11 @@ namespace trifenix.agro.db.model.agro {
         /// <summary>
         /// Nombre de la entidad certificadora.
         /// </summary>
+        /// 
+        [StringSearch(StringRelated.GENERIC_NAME)]
         public override string Name { get; set; }
 
+        [StringSearch(StringRelated.GENERIC_ABBREVIATION)]
         public string Abbreviation { get; set; }
 
     }
