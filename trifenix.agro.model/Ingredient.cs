dc59d6a9318794e52ae282717c42ac8132e4deff
alejandro.iglesias <alejandro.iglesias@trifenix.io>
2020-03-26 18:02:21 -0300

    Ingredient Added

--- trifenix.agro.model/Ingredient.cs
+++ trifenix.agro.model/Ingredient.cs
@@ -1,13 +1,20 @@
 ﻿using Cosmonaut;
 using Cosmonaut.Attributes;
+using trifenix.agro.attr;
+using trifenix.agro.enums.searchModel;
 
 namespace trifenix.agro.db.model.agro {
 
     [SharedCosmosCollection("agro", "Ingredient")]
+    [ReferenceSearch(EntityRelated.INGREDIENT)]
     public class Ingredient : DocumentBaseName, ISharedCosmosEntity {
 
         public override string Id { get; set; }
+
+        [StringSearch(StringRelated.GENERIC_NAME)]
         public override string Name { get; set; }
+
+        [ReferenceSearch(EntityRelated.CATEGORY_INGREDIENT)]
         public string idCategory { get; set; }
 
     }
