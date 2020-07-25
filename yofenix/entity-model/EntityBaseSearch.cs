f5f8303e7efc7b400f09d8b187d8649badb34cc3
agro <alejandro.iglesias@trifenix.io>
2020-07-24 22:57:05 -0400

    Se crean implementaciones de propiedad
    para tests

---
+++ yofenix/entity-model/EntityBaseSearch.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using trifenix.connect.mdm.ts_model;
+
+namespace trifenix.connect.mdm.entity_model
+{
+    public class EntityBaseSearch : IEntitySearch<GeoPointTs>
+    {
+        public string id { get; set; }
+        public DateTime created { get; set; }
+        public int index { get; set; }
+        public IBoolProperty[] bl { get; set; }
+        public IDblProperty[] dbl { get; set; }
+        public IDtProperty[] dt { get; set; }
+        public IEnumProperty[] enm { get; set; }
+        public INum32Property[] num32 { get; set; }
+        public INum64Property[] num64 { get; set; }
+        public IRelatedId[] rel { get; set; }
+        public IStrProperty[] str { get; set; }
+        public IStrProperty[] sug { get; set; }
+        public IProperty<GeoPointTs>[] geo { get; set; }
+    }
+
+
+
+}
