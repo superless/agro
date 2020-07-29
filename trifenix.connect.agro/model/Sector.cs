f3b186be6cc8b7d9848118fb17c44d9453630b30
ale <alejandro.iglesias@trifenix.io>
2020-07-29 13:16:40 -0400

    Se agrega metadata unico para validar que no exista otro

--- trifenix.connect.agro/model/Sector.cs
+++ trifenix.connect.agro/model/Sector.cs
@@ -4,6 +4,7 @@ using trifenix.agro.db;
 using trifenix.connect.agro.index_model.props;
 using trifenix.connect.agro.mdm_attributes;
 using trifenix.connect.mdm.enums;
+using trifenix.connect.mdm_attributes;
 
 namespace trifenix.connect.agro_model
 {
@@ -24,6 +25,7 @@ namespace trifenix.connect.agro_model
         /// nombre
         /// </summary>
         [StringSearch(StringRelated.GENERIC_NAME)]
+        [Unique]
         public override string Name { get; set; }
 
         /// <summary>
