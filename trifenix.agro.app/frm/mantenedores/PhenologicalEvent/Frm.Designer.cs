fd0b9de8933219cef8544d6e12acb8c930897e4f
ale <alejandro.iglesias@trifenix.io>
2020-09-08 16:48:14 -0300

    Creación de Evento fenológico

---
+++ trifenix.agro.app/frm/mantenedores/PhenologicalEvent/Frm.Designer.cs
@@ -0,0 +1,346 @@
+﻿namespace trifenix.agro.app.frm.mantenedores.phenologicalEvent
+{
+    partial class Frm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Frm));
+            this.tbxCorrelativo = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbxName = new System.Windows.Forms.TextBox();
+            this.bsMain = new System.Windows.Forms.BindingSource(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblDescripcion = new System.Windows.Forms.Label();
+            this.gbxItem = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dtEnd = new System.Windows.Forms.DateTimePicker();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dtStart = new System.Windows.Forms.DateTimePicker();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.lbxItems = new System.Windows.Forms.ListBox();
+            this.ValidationForm = new System.Windows.Forms.ErrorProvider(this.components);
+            this.btnDeleteSector = new System.Windows.Forms.Button();
+            this.btnEditSector = new System.Windows.Forms.Button();
+            this.btnAddSector = new System.Windows.Forms.Button();
+            this.pnlButtons = new System.Windows.Forms.Panel();
+            this.pb = new System.Windows.Forms.ProgressBar();
+            this.lblProgress = new System.Windows.Forms.Label();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.bsMain)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.gbxItem.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ValidationForm)).BeginInit();
+            this.pnlButtons.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tbxCorrelativo
+            // 
+            this.tbxCorrelativo.Enabled = false;
+            this.tbxCorrelativo.Location = new System.Drawing.Point(17, 43);
+            this.tbxCorrelativo.Name = "tbxCorrelativo";
+            this.tbxCorrelativo.Size = new System.Drawing.Size(56, 20);
+            this.tbxCorrelativo.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Correlativo";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(86, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Nombre :";
+            // 
+            // tbxName
+            // 
+            this.tbxName.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.bsMain, "Name", true));
+            this.tbxName.Location = new System.Drawing.Point(89, 43);
+            this.tbxName.MaxLength = 200;
+            this.tbxName.Name = "tbxName";
+            this.tbxName.Size = new System.Drawing.Size(333, 20);
+            this.tbxName.TabIndex = 3;
+            this.tbxName.Validated += new System.EventHandler(this.tbxName_Validated);
+            // 
+            // bsMain
+            // 
+            this.bsMain.DataSource = typeof(trifenix.connect.agro_model.PhenologicalEvent);
+            this.bsMain.AddingNew += new System.ComponentModel.AddingNewEventHandler(this.bsMain_AddingNew);
+            this.bsMain.CurrentChanged += new System.EventHandler(this.bsSectors_CurrentChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.lblDescripcion);
+            this.panel1.Location = new System.Drawing.Point(0, -1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(662, 53);
+            this.panel1.TabIndex = 4;
+            // 
+            // lblDescripcion
+            // 
+            this.lblDescripcion.AutoSize = true;
+            this.lblDescripcion.Location = new System.Drawing.Point(12, 10);
+            this.lblDescripcion.Name = "lblDescripcion";
+            this.lblDescripcion.Size = new System.Drawing.Size(63, 13);
+            this.lblDescripcion.TabIndex = 5;
+            this.lblDescripcion.Text = "Descripción";
+            // 
+            // gbxItem
+            // 
+            this.gbxItem.Controls.Add(this.label4);
+            this.gbxItem.Controls.Add(this.dtEnd);
+            this.gbxItem.Controls.Add(this.label3);
+            this.gbxItem.Controls.Add(this.dtStart);
+            this.gbxItem.Controls.Add(this.tbxName);
+            this.gbxItem.Controls.Add(this.tbxCorrelativo);
+            this.gbxItem.Controls.Add(this.btnCancel);
+            this.gbxItem.Controls.Add(this.label1);
+            this.gbxItem.Controls.Add(this.label2);
+            this.gbxItem.Controls.Add(this.btnSave);
+            this.gbxItem.Enabled = false;
+            this.gbxItem.Location = new System.Drawing.Point(213, 58);
+            this.gbxItem.Name = "gbxItem";
+            this.gbxItem.Size = new System.Drawing.Size(437, 338);
+            this.gbxItem.TabIndex = 5;
+            this.gbxItem.TabStop = false;
+            this.gbxItem.Text = "{0}";
+            this.gbxItem.Visible = false;
+            this.gbxItem.Enter += new System.EventHandler(this.gbxItem_Enter);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(119, 69);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Fecha Final";
+            // 
+            // dtEnd
+            // 
+            this.dtEnd.DataBindings.Add(new System.Windows.Forms.Binding("Value", this.bsMain, "EndDate", true));
+            this.dtEnd.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.bsMain, "EndDate", true));
+            this.dtEnd.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtEnd.Location = new System.Drawing.Point(122, 85);
+            this.dtEnd.Name = "dtEnd";
+            this.dtEnd.Size = new System.Drawing.Size(84, 20);
+            this.dtEnd.TabIndex = 11;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(17, 69);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Fecha de Inicio";
+            // 
+            // dtStart
+            // 
+            this.dtStart.DataBindings.Add(new System.Windows.Forms.Binding("Value", this.bsMain, "StartDate", true));
+            this.dtStart.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.bsMain, "StartDate", true));
+            this.dtStart.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtStart.Location = new System.Drawing.Point(20, 85);
+            this.dtStart.Name = "dtStart";
+            this.dtStart.Size = new System.Drawing.Size(84, 20);
+            this.dtStart.TabIndex = 9;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(271, 311);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 8;
+            this.btnCancel.Text = "Cancelar";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnSave.Location = new System.Drawing.Point(352, 311);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(74, 23);
+            this.btnSave.TabIndex = 6;
+            this.btnSave.Text = "Guardar";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // lbxItems
+            // 
+            this.lbxItems.DataSource = this.bsMain;
+            this.lbxItems.DisplayMember = "Name";
+            this.lbxItems.FormattingEnabled = true;
+            this.lbxItems.Location = new System.Drawing.Point(13, 68);
+            this.lbxItems.Name = "lbxItems";
+            this.lbxItems.Size = new System.Drawing.Size(194, 303);
+            this.lbxItems.TabIndex = 9;
+            this.lbxItems.ValueMember = "Id";
+            // 
+            // ValidationForm
+            // 
+            this.ValidationForm.ContainerControl = this;
+            // 
+            // btnDeleteSector
+            // 
+            this.btnDeleteSector.BackgroundImage = global::trifenix.agro.app.Properties.Resources.deleteIcon;
+            this.btnDeleteSector.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnDeleteSector.Location = new System.Drawing.Point(101, 2);
+            this.btnDeleteSector.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.btnDeleteSector.Name = "btnDeleteSector";
+            this.btnDeleteSector.Size = new System.Drawing.Size(23, 19);
+            this.btnDeleteSector.TabIndex = 33;
+            this.btnDeleteSector.UseVisualStyleBackColor = true;
+            // 
+            // btnEditSector
+            // 
+            this.btnEditSector.BackgroundImage = global::trifenix.agro.app.Properties.Resources.edit_icon;
+            this.btnEditSector.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnEditSector.Location = new System.Drawing.Point(74, 3);
+            this.btnEditSector.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.btnEditSector.Name = "btnEditSector";
+            this.btnEditSector.Size = new System.Drawing.Size(23, 19);
+            this.btnEditSector.TabIndex = 32;
+            this.btnEditSector.UseVisualStyleBackColor = true;
+            this.btnEditSector.Click += new System.EventHandler(this.btnEditSector_Click);
+            // 
+            // btnAddSector
+            // 
+            this.btnAddSector.BackgroundImage = global::trifenix.agro.app.Properties.Resources.add_icon;
+            this.btnAddSector.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnAddSector.Location = new System.Drawing.Point(47, 2);
+            this.btnAddSector.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.btnAddSector.Name = "btnAddSector";
+            this.btnAddSector.Size = new System.Drawing.Size(23, 19);
+            this.btnAddSector.TabIndex = 31;
+            this.btnAddSector.UseVisualStyleBackColor = true;
+            this.btnAddSector.Click += new System.EventHandler(this.btnAddSector_Click);
+            // 
+            // pnlButtons
+            // 
+            this.pnlButtons.Controls.Add(this.btnEditSector);
+            this.pnlButtons.Controls.Add(this.btnDeleteSector);
+            this.pnlButtons.Controls.Add(this.btnAddSector);
+            this.pnlButtons.Enabled = false;
+            this.pnlButtons.Location = new System.Drawing.Point(73, 375);
+            this.pnlButtons.Margin = new System.Windows.Forms.Padding(2);
+            this.pnlButtons.Name = "pnlButtons";
+            this.pnlButtons.Size = new System.Drawing.Size(133, 25);
+            this.pnlButtons.TabIndex = 4;
+            // 
+            // pb
+            // 
+            this.pb.Location = new System.Drawing.Point(584, 397);
+            this.pb.Margin = new System.Windows.Forms.Padding(2);
+            this.pb.Name = "pb";
+            this.pb.Size = new System.Drawing.Size(67, 15);
+            this.pb.TabIndex = 9;
+            this.pb.Visible = false;
+            // 
+            // lblProgress
+            // 
+            this.lblProgress.AutoSize = true;
+            this.lblProgress.Location = new System.Drawing.Point(546, 399);
+            this.lblProgress.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblProgress.Name = "lblProgress";
+            this.lblProgress.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.lblProgress.Size = new System.Drawing.Size(35, 13);
+            this.lblProgress.TabIndex = 10;
+            this.lblProgress.Text = "label3";
+            this.lblProgress.Visible = false;
+            // 
+            // Frm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(662, 419);
+            this.Controls.Add(this.lblProgress);
+            this.Controls.Add(this.pb);
+            this.Controls.Add(this.pnlButtons);
+            this.Controls.Add(this.lbxItems);
+            this.Controls.Add(this.gbxItem);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Frm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Evento Fenológico";
+            this.Load += new System.EventHandler(this.SectorFrm_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.bsMain)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.gbxItem.ResumeLayout(false);
+            this.gbxItem.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ValidationForm)).EndInit();
+            this.pnlButtons.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbxCorrelativo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbxName;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblDescripcion;
+        private System.Windows.Forms.GroupBox gbxItem;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.ListBox lbxItems;
+        private System.Windows.Forms.ErrorProvider ValidationForm;
+        private System.Windows.Forms.Button btnDeleteSector;
+        private System.Windows.Forms.Button btnEditSector;
+        private System.Windows.Forms.Button btnAddSector;
+        private System.Windows.Forms.BindingSource bsMain;
+        private System.Windows.Forms.Panel pnlButtons;
+        private System.Windows.Forms.ProgressBar pb;
+        private System.Windows.Forms.Label lblProgress;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DateTimePicker dtEnd;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DateTimePicker dtStart;
+        private System.Windows.Forms.BindingSource bindingSource1;
+    }
+}
