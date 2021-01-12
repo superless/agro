f0278c09875e797b59d903adf4e994085a2b116a
yo-fenix <alejandro.iglesias@trifenix.io>
2021-01-12 16:38:20 -0300

    puto log

---
+++ trifenix.connect.interfaces/log-folder/ILogSimpleQuery.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace trifenix.connect.interfaces.log
+{
+    public interface ILogSimpleQuery
+    {
+        /// <summary>   
+        /// registra las consultas por método.
+        /// </summary>
+        Dictionary<string, List<string>> Queried { get; }
+    }
+}
