069ab190e0c767efcd4e21ce525665e1caa8dc51
yo-fenix <alejandro.iglesias@trifenix.io>
2021-01-25 13:42:56 -0300

    another test pipeline

--- trifenix.agro.functions/CoreFunctions.cs
+++ trifenix.agro.functions/CoreFunctions.cs
@@ -184,7 +184,8 @@ namespace trifenix.agro.functions
 
             // for testing, mostrando en azure el body de la petición.
 
-            var body = await new StreamReader(req.Body).ReadToEndAsync();
+            log.LogInformation("testeando body");
+            var body = await new StreamReader(req.ToString()).ReadToEndAsync();
             log.LogInformation(body);
 
             var result = await GenericMantainer.SendInternalHttp(req, log, s => s.Sector, string.Empty);
